8db7c90950cc25aaac9053d80e6c3e1898d51663
Codevian Developer <developer.codevian@gmail.com>
2021-03-17 14:07:23 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+ import React from 'react';
+import './App.css';
+
+import Poke from "./Pages/Poke"
+
+
+function App() {
+  return (
+    <div>
+
+      <Poke/>
+       
+    </div>
+  )
+}
+
+export default App
